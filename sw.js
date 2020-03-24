164e9ceb9d87b7ab342f7ea9e4b0f6f7b05c084c
babraham123 <babraham123@users.noreply.github.com>
2020-03-24 09:41:00 +0000

    deploy: 5b452992c02b220bbed7431c988fd1c0f7fea2fe

---
+++ sw.js
@@ -0,0 +1,45 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("workbox-v4.3.1/workbox-sw.js");
+workbox.setConfig({modulePathPrefix: "workbox-v4.3.1"});
+
+workbox.core.setCacheNameDetails({prefix: "emergency-site"});
+
+workbox.core.skipWaiting();
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [
+  {
+    "url": "index.html",
+    "revision": "c59709d4aa6b183dec0b40b5a247fe11"
+  },
+  {
+    "url": "posts/2020-03-19-about-this-template/index.html",
+    "revision": "03b54c5b8f9f6cd9ac2902c5da2e2df4"
+  },
+  {
+    "url": "posts/2020-03-22-example-post/index.html",
+    "revision": "865dbeb51449e76f57db0cc2c221efe3"
+  }
+].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerRoute(/^.*\.(html|jpg|png|gif|webp|ico|svg|woff2|woff|eot|ttf|otf|ttc|json)$/, new workbox.strategies.StaleWhileRevalidate(), 'GET');
+workbox.routing.registerRoute(/^https?:\/\/fonts\.googleapis\.com\/css/, new workbox.strategies.StaleWhileRevalidate(), 'GET');
