e20bc05cb72b928fe4621bf6ab5e53ab1f59c91f
Bereket Abraham <babraham42@gmail.com>
2020-03-23 23:32:55 -0700

    Customized readme.

--- src/data/meta.js
+++ src/data/meta.js
@@ -10,12 +10,12 @@ const {
 } = process.env
 
 module.exports = {
-    title: META_TITLE || 'Emergency Site',
-    url: META_URL || '',
-    description: META_DESC || 'An emergency information website.',
+    title: META_TITLE || 'Ethiopian COVID-19 Emergency Site',
+    url: META_URL || 'http://status.ethiopia-covid19.com',
+    description: META_DESC || 'Emergency updates about the COVID-19 outbreak in Ethiopia.',
     lang: META_LANG || 'en',
     primaryColor: META_COLOR || '#DB0000',
-    email: META_EMAIL || undefined,
-    telephone: META_TELEPHONE || undefined,
+    email: META_EMAIL || 'ethiopia.covid19@gmail.com',
+    telephone: META_TELEPHONE || '+01 234 567 890',
     dateFormat: 'dd LLLL yyyy'
 }
